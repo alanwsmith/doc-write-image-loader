28baf118e8e3aef677e1deb7e0b4b6340a2c4697
Alan W. Smith <alan@alanwsmith.com>
2015-12-31 18:48:39 -0500

    Adding initial test for viewport checking.

--- test/tests.js
+++ test/tests.js
@@ -100,3 +100,41 @@ QUnit.test("Verify window.devicePixelRatio is pulled in", function(assert) {
   assert.equal(ip._multiplier, window.devicePixelRatio);
 
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+QUnit.test("Test against multiple size options", function(assert) {
+
+  //////////
+  // Given
+
+  var breakAlpha = { minViewportWidth: 0, maxImageDisplayWidth: 400, quality: 85 };
+  var breakBeta  = { minViewportWidth: 900, maxImageDisplayWidth: 800, quality: 85 };
+
+  var ip = new ImgTagBuilder({ styles: { main: { breakPoints: [ breakAlpha, breakBeta ] } } });
+
+  ip._multiplier = 2; // Force to '2' so testing works across devices.
+  
+  // TODO: Force viewport size.
+
+  // TODO: Duplicate this test setup but target the larger viewport.  
+
+
+  //////////
+  // When
+
+  ip.prep({ image: "horses.jpg", alt: "some horses",  style: "main", maxWidth: 1600, maxHeight: 1000 });
+
+
+  //////////
+  // Then
+
+  // This is the key requirement. Everything else supports it.
+  assert.equal(ip.imgTag(),'<img alt="some horses" class="main" width="400" height="250" src="http://res.cloudinary.com/demo/image/upload/c_fill,q_85,w_800,h_500/horses.jpg">'); 
+
+
+
+
+});
+
