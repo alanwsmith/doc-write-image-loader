13d1aa8820db2cb9c8682da25974a55ae7de6dbb
Alan W. Smith <alan@alanwsmith.com>
2015-12-31 09:39:14 -0500

    Sorted methods and test (no functional changes).

--- doc-write-image-loader.js
+++ doc-write-image-loader.js
@@ -8,17 +8,20 @@ var ImgTagBuilder = function(config) {
   this._style = undefined; 
 };
 
+ImgTagBuilder.prototype.callHeight = function() {
+  return this.callWidth() * this.ratio();
+};
+
+ImgTagBuilder.prototype.callWidth = function() {
+  return this.displayWidth() * this._multiplier;
+};
+
 ImgTagBuilder.prototype.displayHeight = function() {
   return this.callHeight() / this._multiplier;
 };
 ImgTagBuilder.prototype.displayWidth = function() {
   // TODO: Add check to make sure the max source image size will support this display width. Reduce it here if not.
   return this._config.styles[this._style]['breakPoints'][0]['maxImageDisplayWidth'];
-};
-
-ImgTagBuilder.prototype.url = function() {
-  // TODO: Consider making this configurable. 
-  return 'http://res.cloudinary.com/demo/image/upload/c_fill,q_' + this.quality() + ',w_' + this.callWidth() + ',h_' + this.callHeight() + '/' + this._image;
 };
 
 ImgTagBuilder.prototype.imgTag = function() {
@@ -43,16 +46,13 @@ ImgTagBuilder.prototype.quality = function() {
   return this._config.styles[this._style]['breakPoints'][0]['quality'] ? this._config.styles[this._style]['breakPoints'][0]['quality'] : 80;
 }
 
-ImgTagBuilder.prototype.callHeight = function() {
-  return this.callWidth() * this.ratio();
-};
-
-ImgTagBuilder.prototype.callWidth = function() {
-  return this.displayWidth() * this._multiplier;
-};
-
 ImgTagBuilder.prototype.ratio= function() {
   return this._maxHeight / this._maxWidth;
+};
+
+ImgTagBuilder.prototype.url = function() {
+  // TODO: Consider making this configurable. 
+  return 'http://res.cloudinary.com/demo/image/upload/c_fill,q_' + this.quality() + ',w_' + this.callWidth() + ',h_' + this.callHeight() + '/' + this._image;
 };
 
 
