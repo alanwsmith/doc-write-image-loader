1e4682936388a813ba3136cd0cad5c16cf050810
Alan W. Smith <alan@alanwsmith.com>
2016-02-12 07:58:30 -0500

    Added but commented out a default env setup.

--- test/tests.js
+++ test/tests.js
@@ -167,6 +167,12 @@ QUnit.test("Run lots of variaitions for QA", function(assert) {
   //////////
   // Given
 
+  // var defaultTestEnvironment = {
+  //   _devicePixelRatio: 2,
+  //   _innerWidth: 1000,
+  // };
+
+
   var ip = new ImgTagBuilder( { 
     styles: { 
       main: { 
