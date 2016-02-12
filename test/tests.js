e114340f8eb714a1bdaab0f6b64c4d384c7c02cb
Alan W. Smith <alan@alanwsmith.com>
2016-02-12 08:52:21 -0500

    Moved image string to it's own variable.

--- test/tests.js
+++ test/tests.js
@@ -204,8 +204,9 @@ QUnit.test("Run lots of variaitions for QA", function(assert) {
  
     //////////
     // Then
-    
-    assert.equal(ip.imgTag(),'<img alt="some horses" class="main" width="800" height="500" src="http://res.cloudinary.com/demo/image/upload/c_fill,q_85,w_1600,h_1000/horses.jpg">', "Target `img` tag"); 
+
+    var imageString = '<img alt="some horses" class="main" width="800" height="500" src="http://res.cloudinary.com/demo/image/upload/c_fill,q_85,w_1600,h_1000/horses.jpg">';  
+    assert.equal(ip.imgTag(), imageString, "Target `img` tag"); 
 
   }
 
