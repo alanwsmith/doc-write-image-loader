b0326712747479147b4cda1d422cc5ee32461c76
Alan W. Smith <alan@alanwsmith.com>
2016-02-14 20:41:30 -0500

    Stubbed first two steps.

---
+++ object_prototype/features/step_definitions/image_test.rb
@@ -0,0 +1,8 @@
+Given(/^a source image that's (\d+)x(\d+)$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the ratio should be (\d+)\.(\d+)$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
