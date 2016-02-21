ffdee11350c9937a9ba7a60b8a059ee6d8a58d1e
Alan W. Smith <alan@alanwsmith.com>
2016-02-21 12:32:14 -0500

    Alphabetized methods.

--- object_prototype/image.rb
+++ object_prototype/image.rb
@@ -4,16 +4,20 @@ class Image
   attr_accessor :device_pixel_ratio
   attr_reader :call_width
 
-  def call_height
-    source_width * ratio 
-  end
-
   def attribute_height
     call_height / device_pixel_ratio
   end
 
   def attribute_width
     call_width / device_pixel_ratio
+  end
+
+  def call_height
+    source_width * ratio 
+  end
+
+  def ratio
+    source_height.to_f / source_width.to_f
   end
 
   def request_width width
@@ -24,9 +28,5 @@ class Image
     @call_width = params[:width] * device_pixel_ratio
   end
   
-  def ratio
-    source_height.to_f / source_width.to_f
-  end
-
 end
 
