cce2b77dbdbc51057a437ba37b718916fc14ac0b
Alan W. Smith <alan@alanwsmith.com>
2016-02-21 11:55:13 -0500

    Made attribute_width dynamic again.

--- object_prototype/image.rb
+++ object_prototype/image.rb
@@ -2,9 +2,7 @@ class Image
   
   attr_accessor :source_width, :source_height
   attr_accessor :desired_width, :device_pixel_ratio
-  attr_accessor :attribute_width
   attr_accessor :call_width, :call_height
-
 
   def call_height
     source_width * ( source_height.to_f / source_width.to_f )
@@ -14,9 +12,12 @@ class Image
     source_height
   end
 
+  def attribute_width
+    source_width
+  end
+
   def request params
     @call_width = params[:width] 
-    @attribute_width = params[:width]
   end
 
 end
