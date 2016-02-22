6074f6ba33187c37ed5163ac6723cde4063ab0ae
Alan W. Smith <alan@alanwsmith.com>
2016-02-21 23:30:15 -0500

    Finished moving to using requested attribute with as the core.

--- object_prototype/image.rb
+++ object_prototype/image.rb
@@ -1,8 +1,8 @@
 class Image
 
-  # The approach is to base everything off the call width. 
-  # Even when a height is requested, it's translated into a
-  # base call_width so the rest of the math only has
+  # The approach is to base everything off the attribute
+  # width.  Even when a height is requested, it's translated 
+  # into a base call_width so the rest of the math only has
   # to be done in one way.
 
   # Values are always retured as integers using a "floor" mechanic
@@ -10,8 +10,7 @@ class Image
   # "601". This way, a simple crop can be applied to the source
   # pixels instead of upsizing by one pixel. 
 
-  attr_accessor :call_width, :device_pixel_ratio, :source_width, :source_height
-  attr_accessor :attribute_width
+  attr_accessor :attribute_width, :device_pixel_ratio, :source_width, :source_height
 
   def attribute_height
     call_height / device_pixel_ratio
@@ -21,18 +20,20 @@ class Image
     (call_width * ratio).to_i
   end
 
+  def call_width
+    attribute_width * device_pixel_ratio
+  end
+
   def ratio
     source_height.to_f / source_width.to_f
   end
   
   def request_height height
     @attribute_width = (height * source_width) / source_height 
-    @call_width = ((height * source_width) / source_height) * device_pixel_ratio
   end
 
   def request_width width
     @attribute_width = width
-    @call_width = width * device_pixel_ratio
   end
 
 end
