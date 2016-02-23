604ab399e68b8384c7d572c707fdface53a7e418
Alan W. Smith <alan@alanwsmith.com>
2016-02-22 21:56:44 -0500

    Removed deprecated instance vars.

--- object_prototype/image_tag_builder.rb
+++ object_prototype/image_tag_builder.rb
@@ -1,13 +1,8 @@
 class ImageTagBuilder
  
   attr_accessor :image_type
-  attr_accessor :window_device_pixel_ratio, :window_inner_width, :window_inner_height
-
 
   def initialize_with params
-    @window_inner_width = params[:window_inner_width]
-    @window_inner_height = params[:window_inner_height]
-    @window_device_pixel_ratio = params[:window_device_pixel_ratio]
 
     @size_finder = SizeFinder.new_with(params)
     @size_finder.load_basic_tests
