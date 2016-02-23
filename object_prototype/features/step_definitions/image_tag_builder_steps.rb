5be358fd16b5f630f4fa00c14e239b40bb570b7c
Alan W. Smith <alan@alanwsmith.com>
2016-02-22 21:43:16 -0500

    Removed deprecated step definitions.

--- object_prototype/features/step_definitions/image_tag_builder_steps.rb
+++ object_prototype/features/step_definitions/image_tag_builder_steps.rb
@@ -12,42 +12,8 @@ Given(/^Viewport: (\d+)x(\d+) \- DPR: (\d+) \- Source: (\d+)x(\d+)$/) do |view_w
   )
 end
 
-Given(/^a viewport that's (\d+)x(\d+)$/) do |width, height|
-  @i.window_inner_width = width.to_i
-  @i.window_inner_height= height.to_i
-end
-
-Given(/^a window\.devicePixelRatio of (\d+)$/) do |ratio|
-  @i.window_device_pixel_ratio = ratio.to_i
-end
-
-Given(/^a source image that's (\d+)x(\d+)$/) do |width, height|
-  @i.source_width = width.to_i
-  @i.source_height = height.to_i
-end
-
 Given(/^a type of (.*?)$/) do |type|
   @i.image_type = type
-end
-
-Then(/^the inner width should be (\d+)$/) do |width|
-  expect(@i.window_inner_width).to eq(width.to_i)
-end
-
-Then(/^the inner height should be (\d+)$/) do |height|
-  expect(@i.window_inner_height).to eq(height.to_i)
-end
-
-Then(/^the DPR should be (\d+)$/) do |dpr|
-  expect(@i.window_device_pixel_ratio).to eq(dpr.to_i)
-end
-
-Then(/^the source image width should be (\d+)$/) do |width|
-  expect(@i.source_width).to eq(width.to_i)
-end
-
-Then(/^the source image height should be (\d+)$/) do |height|
-  expect(@i.source_height).to eq(height.to_i)
 end
 
 Then(/^the width attribute should be (x?)(\d+)$/) do |skip, width|
