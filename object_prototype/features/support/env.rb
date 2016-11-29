15427a390678d72ad1237941648bd2e35d16b3e7
Alan W. Smith <alan@alanwsmith.com>
2016-11-29 10:38:12 -0500

    Removed original prototype to setup for new run.

--- object_prototype/features/support/env.rb
+++
@@ -1,5 +0,0 @@
-$LOAD_PATH << File.expand_path('../../../', __FILE__)
-require 'image'
-require 'image_tag_builder'
-require 'size_finder'
-
--- object_prototype_v2/features/support/env.rb
+++
@@ -1,4 +0,0 @@
-$LOAD_PATH << File.expand_path('../../../', __FILE__)
-require "image_tag_builder"
-
-
