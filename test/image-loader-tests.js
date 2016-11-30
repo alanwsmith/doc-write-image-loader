0d42c045b9021a2fe5dbff61ef1532a77574a480
Alan W. Smith <alan@alanwsmith.com>
2016-11-30 16:23:10 -0500

    Cleaned up comments.

--- test/image-loader-tests.js
+++ test/image-loader-tests.js
@@ -1,9 +1,9 @@
 QUnit.test("2x DPR Basic call stright to load_params", function(assert) {
 
-  // Given:
+  // Given
 	var imageLoader = new ImageLoader(); 
 
-  // When:
+  // When
   imageLoader.load_params(
     {
     	dpr: 2,
@@ -18,7 +18,7 @@ QUnit.test("2x DPR Basic call stright to load_params", function(assert) {
     }
   );
 
-  // Then:  Verify instance variables.
+  // Then - Verify instance variables.
   assert.equal(imageLoader._dpr, 2, "Device pixel resolution"); 
   assert.equal(imageLoader._image_name, "horses.jpg", "Image name"); 
   assert.equal(imageLoader._raw_height, 1067, "Raw image height"); 
@@ -29,7 +29,7 @@ QUnit.test("2x DPR Basic call stright to load_params", function(assert) {
   assert.equal(imageLoader._viewport_width, 1024, "Viewport width");
   assert.equal(imageLoader._url_template, "http://res.cloudinary.com/demo/image/upload/w_[WIDTH],h_[HEIGHT],q_[QUALITY]/[IMAGE_NAME]", "URL Template");
 
-  // And: Verify functions
+  // And - Verify functions
   assert.equal(imageLoader.url_to_call(), "http://res.cloudinary.com/demo/image/upload/w_1024,h_682,q_80/horses.jpg", "Final URL");
   assert.equal(imageLoader.render_height(), 341, "Render height");
   assert.equal(imageLoader.render_width(), 512, "Render width");
