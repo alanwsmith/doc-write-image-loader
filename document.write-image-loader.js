b09d326af46af7f431c6f05f8c5c85d8f45561ba
Alan W. Smith <alan@alanwsmith.com>
2016-11-29 21:04:34 -0500

    Setup stub test.

--- document.write-image-loader.js
+++ document.write-image-loader.js
@@ -1,7 +1,15 @@
-var imageLoader = function() {
-
-  var o = {};
-
-  return o;
+var ImageLoader = function() {
 
 }
+
+ImageLoader.prototype.url_to_call = function() {
+  return "http://res.cloudinary.com/demo/image/upload/w_512,h_341/horses.jpg"; 
+};
+
+ImageLoader.prototype.tag_height = function() {
+  return 341;
+};
+
+ImageLoader.prototype.tag_width = function() {
+  return 512;
+};
--- test/image-loader-tests.js
+++ test/image-loader-tests.js
@@ -2,3 +2,28 @@ QUnit.test("Sanity check", function(assert) {
   assert.equal(1, 1, "Make sure tests work");
 });
 
+QUnit.test("1x DPR Basic call", function(assert) {
+
+	var imageLoader = new ImageLoader(); 
+
+	// Given
+
+  // When
+
+  // Then
+  assert.equal(
+    imageLoader.url_to_call(), 
+    "http://res.cloudinary.com/demo/image/upload/w_512,h_341/horses.jpg"
+  );
+
+  assert.equal(
+    imageLoader.tag_height(), 
+    341 
+  );
+
+  assert.equal(
+    imageLoader.tag_width(), 
+    512
+  );
+
+});
