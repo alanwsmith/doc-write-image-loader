819d06353de235d64f2a2ff5a508f240e8d9bfe6
Alan W. Smith <alan@alanwsmith.com>
2017-09-18 15:45:20 -0400

    Removed `q_` from stubbed output.

--- document-write-image-loader-0-5-x/document-write-image-loader.js
+++ document-write-image-loader-0-5-x/document-write-image-loader.js
@@ -8,7 +8,7 @@ ImageLoader_0_5_x.prototype.version_number = function() {
 
 
 ImageLoader_0_5_x.prototype.image_string_from_params = function() {
-    return '<img alt="image description" width="640" height="436" src="http://res.cloudinary.com/demo/image/upload/w_1280,h_852,q_80/horses.jpg">';
+    return '<img alt="image description" width="640" height="436" src="http://res.cloudinary.com/demo/image/upload/w_1280,h_852/horses.jpg">';
 };
 
 ImageLoader_0_5_x.prototype.load_environmental_params = function() {
