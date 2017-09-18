1df2832806e1da5bcc641df37206b34f301016ae
Alan W. Smith <alan@alanwsmith.com>
2017-09-18 15:48:27 -0400

    Updated to use actual alt description in basic integration test.

--- tests/document-write-image-loader-0-5-x-tests.js
+++ tests/document-write-image-loader-0-5-x-tests.js
@@ -21,7 +21,7 @@ QUnit.test("Make sure the version number is accurate", function(assert) {
 QUnit.test("Integration Test: Basic call verification", function(assert) {
 
     // Preflight
-    var target_string = '<img alt="image description" width="640" height="436" src="http://res.cloudinary.com/demo/image/upload/w_1280,h_852/horses.jpg">';
+    var target_string = '<img alt="Photo of Hourses" width="640" height="436" src="http://res.cloudinary.com/demo/image/upload/w_1280,h_852/horses.jpg">';
 
     // Given
     var imageLoader = new ImageLoader_0_5_x(); 
