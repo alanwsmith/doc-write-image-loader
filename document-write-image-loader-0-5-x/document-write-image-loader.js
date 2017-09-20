1f81fc512a8ac8f83f31036e63580de1b0492620
Alan W. Smith <alan@alanwsmith.com>
2017-09-19 21:50:15 -0400

    Translated `.calculate_logical_height()` prior logic to new names.

--- document-write-image-loader-0-5-x/document-write-image-loader.js
+++ document-write-image-loader-0-5-x/document-write-image-loader.js
@@ -81,7 +81,7 @@ ImageLoader_0_5_x.prototype.url_request_width = function() {
 ImageLoader_0_5_x.prototype.calculate_logical_height = function() {
 
     // Prior logic
-	// Math.floor(this._raw_height * this.render_width() / this._raw_width );
+	// Math.floor([source_file_height] * [logical_width] / [source_file_width]);
 
     // SAFETY VALVE - To get back to green quickly if necessary
     return_value = 436;
