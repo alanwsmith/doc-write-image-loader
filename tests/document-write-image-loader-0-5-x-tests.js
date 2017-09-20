175ee82d4c60d88a6260f0f86e42a9d919afa9ef
Alan W. Smith <alan@alanwsmith.com>
2017-09-19 21:50:37 -0400

    Switched to `source_file_` prefixes.

--- tests/document-write-image-loader-0-5-x-tests.js
+++ tests/document-write-image-loader-0-5-x-tests.js
@@ -29,9 +29,8 @@ QUnit.test("Integration Test 1: Base functionality using the minimum setup and c
     var result_string = this.image_loader.image_string_from_params( {
         filename: "horses.jpg", 
         alt_text: "Photo of Horses",
-		// TODO: Rename these to `max_physical_width` and `max_physical_height`.
-        source_width: 1600,
-        source_height: 1067
+        source_file_width: 1600,
+        source_file_height: 1067
     });
 
     // Then
