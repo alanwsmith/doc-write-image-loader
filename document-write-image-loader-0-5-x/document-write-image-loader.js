692ca62ecbbe0cb5fad882fd47f5578f137c4a55
Alan W. Smith <alan@alanwsmith.com>
2017-09-19 21:07:50 -0400

    Starting to build logic for `.calculate_logical_height()`.

--- document-write-image-loader-0-5-x/document-write-image-loader.js
+++ document-write-image-loader-0-5-x/document-write-image-loader.js
@@ -80,7 +80,10 @@ ImageLoader_0_5_x.prototype.url_request_width = function() {
 
 ImageLoader_0_5_x.prototype.calculate_logical_height = function(params) {
 
-    var return_value = 436;
+    // Prior logic
+	// Math.floor(this._raw_height * this.render_width() / this._raw_width );
+
+    var return_value = Math.floor(436);
 
     return return_value;
 
