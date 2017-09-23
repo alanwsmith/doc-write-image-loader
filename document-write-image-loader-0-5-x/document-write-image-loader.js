58d4135fa6907b2d746c5fa5e38fc2ac1f3c8cec
Alan W. Smith <alan@alanwsmith.com>
2017-09-23 10:38:55 -0400

    Formatting cleanup recommended by JSLint.

--- document-write-image-loader-0-5-x/document-write-image-loader.js
+++ document-write-image-loader-0-5-x/document-write-image-loader.js
@@ -39,7 +39,7 @@ ImageLoader_0_5_x.prototype.viewport_logical_width = function () { return this._
 /************************************************************\
  * Unit Functions
 \************************************************************/
-ImageLoader_0_5_x.prototype.image_tag_string = function() {
+ImageLoader_0_5_x.prototype.image_tag_string = function () {
     var return_value = this.img_tag_template();
     return_value = return_value.replace("[ALT_TEXT]", this.alt_text());
     return_value = return_value.replace("[LOGICAL_WIDTH_FOR_ATTRIBUTE]", this.logical_width());
@@ -47,7 +47,7 @@ ImageLoader_0_5_x.prototype.image_tag_string = function() {
     return_value = return_value.replace("[SOURCE_URL]", this.url_string());
     return return_value;
 }
-ImageLoader_0_5_x.prototype.image_tag_string_from_params = function(params) {
+ImageLoader_0_5_x.prototype.image_tag_string_from_params = function (params) {
     // Conveince method that builds a string from params
     this._alt_text = params["alt_text"];
     this._filename = params["filename"];
@@ -56,18 +56,18 @@ ImageLoader_0_5_x.prototype.image_tag_string_from_params = function(params) {
     var return_value = this.image_tag_string();
     return return_value;
 };
-ImageLoader_0_5_x.prototype.load_environment = function() {
+ImageLoader_0_5_x.prototype.load_environment = function () {
     this._dpr = window.devicePixelRatio ? window.devicePixelRatio : 1;
     this._viewport_logical_width = window.innerWidth;
     this._viewport_logical_height = window.innerHeight;
 };
-ImageLoader_0_5_x.prototype.logical_height = function() {
+ImageLoader_0_5_x.prototype.logical_height = function () {
     var return_value = Math.floor(
         this.raw_source_physical_height() * this.logical_width() / this.raw_source_physical_width()
     );
     return return_value;
 };
-ImageLoader_0_5_x.prototype.logical_width = function() {
+ImageLoader_0_5_x.prototype.logical_width = function () {
     var return_value = Math.min(
         this.raw_source_dpr_max_logical_width(),
         this.viewport_percentage_max_logical_width()
@@ -76,39 +76,39 @@ ImageLoader_0_5_x.prototype.logical_width = function() {
     return_value = (Math.floor(return_value / 10) * 10);
     return return_value;
 };
-ImageLoader_0_5_x.prototype.physical_height_to_call = function() {
+ImageLoader_0_5_x.prototype.physical_height_to_call = function () {
     var return_value = Math.floor(
         this.logical_height() * this.dpr()
     );
     return return_value;
 };
-ImageLoader_0_5_x.prototype.physical_width_to_call = function() {
+ImageLoader_0_5_x.prototype.physical_width_to_call = function () {
     var return_value = Math.floor(
         this.logical_width() * this.dpr()
     );
 	return return_value;
 };
-ImageLoader_0_5_x.prototype.raw_source_dpr_max_logical_width = function() {
+ImageLoader_0_5_x.prototype.raw_source_dpr_max_logical_width = function () {
     var return_value = Math.floor(
         this.raw_source_physical_width() / this.dpr()
     );
     return return_value;
 };
-ImageLoader_0_5_x.prototype.url_string = function() {
+ImageLoader_0_5_x.prototype.url_string = function () {
     var return_value = this.url_template();
     return_value = return_value.replace("[PHYSICAL_WIDTH_TO_CALL]", this.physical_width_to_call());
     return_value = return_value.replace("[PHYSICAL_HEIGHT_TO_CALL]", this.physical_height_to_call());
     return_value = return_value.replace("[FILENAME]", this.filename());
     return return_value;
 };
-ImageLoader_0_5_x.prototype.viewport_percentage_max_logical_width = function() {
+ImageLoader_0_5_x.prototype.viewport_percentage_max_logical_width = function () {
     var return_value = Math.floor(
         this.max_percent_of_viewport_logical_width() * 0.01 * this.viewport_logical_width()
     );
     return return_value;
 };
 // TODO: Figure out a way to test this output.
-ImageLoader_0_5_x.prototype.write_image = function(params) {
+ImageLoader_0_5_x.prototype.write_image = function (params) {
     document.write(this.image_tag_string_from_params({
         alt_text: params["alt_text"],
         filename: params["filename"],
